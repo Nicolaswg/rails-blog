836ad73372adf81c39ce948fb26a1f077cc857f5
Nigonbol123@gmail.com <nigonbol123@gmail.com>
2022-04-27 12:46:57 -0400

    Add user controller root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+	root 'user#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+	resources :user
   # Defines the root path route ("/")
   # root "articles#index"
 end
