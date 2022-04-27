ddccce69ea50a83287ef9c611eec166afc2f675e
Nigonbol123@gmail.com <nigonbol123@gmail.com>
2022-04-27 12:58:12 -0400

    Add Post controller routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
 	root 'user#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-	resources :user, only: [:index, :show]
+	resources :user, only: [:index, :show] do
+		resources :post, only: [:index, :show]
+	end
   # Defines the root path route ("/")
   # root "articles#index"
 end
