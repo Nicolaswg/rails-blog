3f1f4544bf535c4b230e25e7ff7bad6fe4bc2bfb
Nigonbol123@gmail.com <nigonbol123@gmail.com>
2022-04-27 12:51:53 -0400

    Add User controller routes for index || show methods

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 	root 'user#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-	resources :user
+	resources :user, only: [:index, :show]
   # Defines the root path route ("/")
   # root "articles#index"
 end
