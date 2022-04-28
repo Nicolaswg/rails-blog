36818767eb2db4e732854e8a056126342ba095c3
Nigonbol123@gmail.com <nigonbol123@gmail.com>
2022-04-27 22:14:49 -0400

    Update tests description

--- spec/requests/user_spec.rb
+++ spec/requests/user_spec.rb
@@ -12,13 +12,13 @@ RSpec.describe 'UserController', type: :request do
       expect(response).to render_template(:index)
     end
 
-    it 'success response for /user' do
+    it 'success response for :index' do
       get '/user'
       expect(response).to have_http_status(:ok)
       expect(response).to render_template(:index)
     end
 
-    it 'success response for current user' do
+    it 'success response for :show' do
       get '/user/1'
       expect(response).to have_http_status(:ok)
       expect(response).to render_template(:show)
