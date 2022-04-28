6b27ca8b867c73d4dd90a8a83f638de42e26c6a2
Nigonbol123@gmail.com <nigonbol123@gmail.com>
2022-04-27 22:01:14 -0400

    Add end of line rubocop linter

---
+++ spec/requests/user_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe 'UserController', type: :request do
+  describe 'GET UserController response' do
+    it 'Test root path' do
+      get '/'
+      expect('/').to eq(root_path)
+    end
+
+    it 'render root website' do
+      get '/'
+      expect(response).to render_template(:index)
+    end
+
+    it 'success response for /user' do
+      get '/user'
+      expect(response).to have_http_status(:ok)
+      expect(response).to render_template(:index)
+    end
+
+    it 'success response for current user' do
+      get '/user/1'
+      expect(response).to have_http_status(:ok)
+      expect(response).to render_template(:show)
+    end
+  end
+end
