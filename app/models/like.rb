052069a8ac21d99b6f1a6d1f38436cec84caa0ab
Nigonbol123@gmail.com <nigonbol123@gmail.com>
2022-05-03 08:54:17 -0400

    Add like model custom method

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,11 @@
 class Like < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post, class_name: 'Post'
+
+	after_save :update_post_likes_counter
+
+
+	def update_post_comment_counter
+		post.increment!(:likescounter)
+	end
 end
