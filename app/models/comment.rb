c6ddf8f940df593abafe450254ae9972d278ca7c
Nigonbol123@gmail.com <nigonbol123@gmail.com>
2022-05-03 08:53:32 -0400

    Add comment model custom method

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,12 @@
 class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post, class_name: 'Post'
+
+	after_save :update_post_comment_counter
+
+
+	def update_post_comment_counter
+		post.increment!(:commentscounter)
+	end
+	
 end
