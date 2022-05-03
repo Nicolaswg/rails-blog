e094609bd5943a92162d2f174126fe6c3f345fff
Nigonbol123@gmail.com <nigonbol123@gmail.com>
2022-05-03 00:09:21 -0400

    Update models associations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-	belongs_to :author, class_name: 'User'
-	has_many :likes
-	has_many :comments
+  belongs_to :author, class_name: 'User'
+  has_many :likes, class_name: 'Like'
+  has_many :comments, class_name: 'Comment'
 end
