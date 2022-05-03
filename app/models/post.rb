10cc94c7bd7b9d67a4a6943aa0fc48da3df28785
Nigonbol123@gmail.com <nigonbol123@gmail.com>
2022-05-03 08:54:36 -0400

    Add post model custom method

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,8 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :likes, class_name: 'Like'
   has_many :comments, class_name: 'Comment'
+
+	def most_recent_comments
+		comments.limit(5).order(created_at: :desc)
+	end
 end
