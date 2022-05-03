a97130c24fcf3afbc1c4c261565cfe1ec0cf5086
Nigonbol123@gmail.com <nigonbol123@gmail.com>
2022-05-03 12:26:40 -0400

    Update like model||post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,12 @@ class Post < ApplicationRecord
   has_many :likes, class_name: 'Like'
   has_many :comments, class_name: 'Comment'
 
+	after_save :update_post_counter
+
+	def update_post_counter
+		author.increment!(:post_counter)
+	end
+
 	def most_recent_comments
 		comments.limit(5).order(created_at: :desc)
 	end
