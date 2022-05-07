53583a1794ec3be86ae54837cb375e016185e468
Nigonbol123@gmail.com <nigonbol123@gmail.com>
2022-05-06 22:38:12 -0400

    Add flash message to like success

--- app/controllers/like_controller.rb
+++ app/controllers/like_controller.rb
@@ -3,7 +3,7 @@ class LikeController < ApplicationController
     post = Post.find(params[:post_id])
     user = current_user
     like = Like.new(author_id: user.id, post_id: post.id)
-
+    flash[:success] = 'Like added' if like.save
     redirect_to "/user/#{post.author.id}/post/#{post.id}" if like.save
   end
 end
